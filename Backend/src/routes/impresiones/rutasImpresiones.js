1abb053b97e8ac62dd466e6424883cec26b40f25
agusv <agusvarela5@gmail.com>
2023-10-06 22:39:29 -0300

    Rutas. Creadas rutas para detalle de impresion, traer todas las impresiones y traer todos los filamentos.

--- Backend/src/routes/impresiones/rutasImpresiones.js
+++ Backend/src/routes/impresiones/rutasImpresiones.js
@@ -1,10 +1,12 @@
 const Router = require ("express")
 
 const postNuevaImpresionhandler = require("../../handlers/postNuevaImpresionHandler")
+const {handlerGetImpresiones,handlerGetImpresionPorId}=require('../../handlers/impresiones/handlerGetImpresiones')
 
 const impresiones = Router()
 
+impresiones.get('/',handlerGetImpresiones)
+impresiones.get('/:id',handlerGetImpresionPorId)
 impresiones.post("/", postNuevaImpresionhandler)
-
 
 module.exports = impresiones
