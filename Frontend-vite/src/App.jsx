48f42e40a42f0e9b362d7f089e7769f926c598cc
agusv <agusvarela5@gmail.com>
2023-10-17 21:03:18 -0300

    correccion de router.

--- Frontend-vite/src/App.jsx
+++ Frontend-vite/src/App.jsx
@@ -1,17 +1,16 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Router} from "react-router-dom"
 import Landing from "./views/Landing/Landing"
 import Home from "./views/Home/Home"
-import './App.css'
 
 function App() {
 
   return (
-    <div>
+    
      <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/impresiones" element={<Home/>}/>
      </Routes>
-    </div>
+   
   )
 }
 
--- Frontend-vite/src/main.jsx
+++ Frontend-vite/src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import {configureStore} from "@reduxjs/toolkit"
 import rootReducer from "./Redux/rootReducer.js"
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 axios.defaults.baseURL = "http://localhost:3001/asImpresiones"
 
@@ -16,7 +17,9 @@ const store = configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
