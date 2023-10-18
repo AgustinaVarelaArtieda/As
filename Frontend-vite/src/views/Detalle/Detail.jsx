d21d5ad69c47012d8f7fab705dfc997dbc455b48
agusv <agusvarela5@gmail.com>
2023-10-17 21:00:50 -0300

    creacion detail.

---
+++ Frontend-vite/src/views/Detalle/Detail.jsx
@@ -0,0 +1,11 @@
+import DetalleImpresion from "../../components/Detalles/DetalleImpresion";
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function Detail(){
+    return(
+        <div>
+            <NavBar/>
+            <DetalleImpresion/>
+        </div>
+    )
+}
