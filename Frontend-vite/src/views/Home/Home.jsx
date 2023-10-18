221aa4d79e7a879a9701c752aa3f26e16bb91c7b
agusv <agusvarela5@gmail.com>
2023-10-17 21:00:40 -0300

    creacion home.

---
+++ Frontend-vite/src/views/Home/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar"
+
+function Home(){
+    return(
+        <div>
+            <h1>HOLA</h1>
+            <NavBar/>
+            
+        </div>
+    )
+}
+
+export default Home;
