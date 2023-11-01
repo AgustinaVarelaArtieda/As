f7e90f2283d3df868fd1f344b0781cbeb7f53b92
agusv <agusvarela5@gmail.com>
2023-10-31 22:27:03 -0300

    Creacion de seccion view Perfil del ususario.

---
+++ Frontend-react/src/views/Perfil/PerfilUsuario.jsx
@@ -0,0 +1,11 @@
+import MisCompras from "../../components/Compras/MisCompras";
+import Usuario from "../../components/Usuario/Usuario";
+
+export default function PerfilUsuario(){
+    return(
+        <div>
+            <h1>Perfil de Usuario</h1>
+            <Usuario/>
+        </div>
+    )
+}
