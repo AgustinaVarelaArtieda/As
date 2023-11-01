cdae317b24340d727804b01e3c1566238fbce1d1
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-10-31 22:22:05 -0300

    el handler de nuevo usuario no era asincronico

--- Backend/src/handlers/Usuarios/handlerNuevoUsuario.js
+++ Backend/src/handlers/Usuarios/handlerNuevoUsuario.js
@@ -1,9 +1,10 @@
 const nuevoUsuario=require('../../controllers/Usuarios/nuevoUsuario')
 
-const nuevoUsuarioHandler=(req,res,next)=>{
+const nuevoUsuarioHandler= async(req,res,next)=>{
     try {
         const {nombre,idAuth,email,avatar}=req.body
-        const usuario=nuevoUsuario(nombre,idAuth,email,avatar)
+        const usuario= await nuevoUsuario(nombre,idAuth,email,avatar)
+        console.log(usuario)
         return res.status(200).json(usuario)
         
     } catch (error) {
