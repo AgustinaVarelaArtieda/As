4952d1906f454e07fb911eaed7e7f5f3c38ea21c
agusv <agusvarela5@gmail.com>
2023-10-31 22:23:21 -0300

    Se modifico la forma de busqueda.

--- Backend/src/handlers/Usuarios/handlerInfoUsuario.js
+++ Backend/src/handlers/Usuarios/handlerInfoUsuario.js
@@ -1,9 +1,9 @@
 const infoUsuario = require("../../controllers/Usuarios/infoUsuario")
 
-const infoUsuarioHandler=(req,res,next)=>{
+const infoUsuarioHandler=async (req,res,next)=>{
     try {
-        const {id}=req.params
-        const usuario=infoUsuario(id)
+        const {idAuth}=req.params
+        const usuario=await infoUsuario(idAuth)
         return res.status(200).json(usuario)
     } catch (error) {
         next(error)
