583cfadc43f12cf8b0e058e40ed71c86869d6193
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-11-07 17:31:27 -0300

    logica de serachbar funcional

--- Frontend-react/src/components/Cards/Cards.jsx
+++ Frontend-react/src/components/Cards/Cards.jsx
@@ -2,22 +2,33 @@ import axios from "axios"
 import {useState, useEffect} from "react"
 import Card from "./Card"
 import Paginacion from "../Paginacion/Paginacion"
+import {useSelector} from "react-redux"
 function Cards () {
     
+    const busqueda = useSelector(state => state.search)
+
     const [impresiones, setImpresiones] = useState([]);
 
     const [paginaActual, setPaginaActual]=useState(1)
     const impresionesPorPagina=10
     const totalImpresiones=20   //Ver como calcular esto (ver de hacer una funcion que me traiga solo la cantidad de elementos que hay en la carpeta impresiones)
 
-    const handleImpresion = async()=>{
-        const response = await axios.get(`/impresiones?numeroPagina=${paginaActual}&cantidadPorPagina=${impresionesPorPagina}`)
-        setImpresiones(response.data)
-    }
+    const handleImpresion = async () => {
+        if (busqueda) {
+          const response = await axios.get(`/impresiones?nombre=${busqueda.searchInput}&numeroPagina=${paginaActual}&cantidadPorPagina=${impresionesPorPagina}`)
+          setImpresiones(response.data);
+        } else {
+          const response = await axios.get(`/impresiones?numeroPagina=${paginaActual}&cantidadPorPagina=${impresionesPorPagina}`
+          );
+          setImpresiones(response.data);
+        }
+      };
+        
 
     useEffect(()=>{
+        console.log(busqueda)
         handleImpresion()
-    })
+    },[busqueda])
 
     function paginaFuncion(numero){
         setPaginaActual(numero)
