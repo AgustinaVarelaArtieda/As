9ea2381e4e8c4fe72737791eda2a91f6781f4eb8
agusv <agusvarela5@gmail.com>
2023-11-22 00:29:32 -0300

    feat(frontend): creacion de contacto para impresiones al mayor

---
+++ Frontend-react/src/components/Contacto/contactoCompras.jsx
@@ -0,0 +1,11 @@
+function ComprasMayor(){
+    return(
+        <div>
+            <h1>Compras por mayor</h1>
+            <p>Si deseas realizar compras mayores a 10 impresiones contactanos con nosotros!</p>
+            <button>Contactar</button>
+        </div>
+    )
+}
+
+export default ComprasMayor
