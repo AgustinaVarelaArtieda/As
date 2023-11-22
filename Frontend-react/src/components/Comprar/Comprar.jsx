df932488d2392120b43a07b37ace473c7d1cecfa
agusv <agusvarela5@gmail.com>
2023-11-22 00:29:07 -0300

    feat(frontend): agregada la opcion de agregar al carrito

--- Frontend-react/src/components/Comprar/Comprar.jsx
+++ Frontend-react/src/components/Comprar/Comprar.jsx
@@ -16,10 +16,16 @@ export default function Comprar({precio,id ,nombre, cantidad, imagen, estoyEnCar
         id: id
     }]
 
+    const agrCarrito={
+        id: id,
+        solicitud: "agregar"
+    }
+
     useEffect(()=>{
         if (estoyEnCarrito === true){
             setEnCarrito(true)
         }
+        // eslint-disable-next-line
     },[])
 
     async function comprarImpresion(usuario){
@@ -32,15 +38,26 @@ export default function Comprar({precio,id ,nombre, cantidad, imagen, estoyEnCar
         }
     }
 
-    function agregarCarrito(){
+    async function agregarCarrito(usuario){
         //funcion para agregar producto al carrito
+        if(!usuario){
+            alert("no te registraste boludon")
+        }else{
+            const response =await axios.put(`/carrito/${usuario.idAuth}`, agrCarrito)
+            if(response.status===200){
+                setEnCarrito(true)
+                alert("Se agrego 1 impresion al carrito")
+            }else{
+                alert("error al agregar al carrito")
+            }
+        }
     }
 
     return(
         <div>
             <h1>{precio}</h1>
             <button onClick={()=>comprarImpresion(usuario)}>Comprar</button>
-            {enCarrito?'':<button onClick={()=>agregarCarrito}>Agregar al Carrito</button>}
+            {enCarrito?<p>Producto en carrito</p>:<button onClick={()=>agregarCarrito(usuario)}>Agregar al Carrito</button>}
         </div>
     )
 }
