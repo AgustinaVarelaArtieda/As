d0ad0c0caee818035b7d9fcc50bd196511377b85
agusv <agusvarela5@gmail.com>
2024-01-08 21:05:26 -0300

    style(frontend): aplicacion de colores e implementacion de framework

--- Frontend-react/src/components/LogOut/Logout.jsx
+++ Frontend-react/src/components/LogOut/Logout.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {cache} from "../NavBar/NavBar"
+import Button from "@mui/material/Button";
+import LogoutIcon from '@mui/icons-material/Logout';
+
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
@@ -9,9 +12,9 @@ const LogoutButton = () => {
     cache.remove("usuario")
   }
   return (
-    <button onClick={()=> handleLogOut()}>
+    <Button variant="text" onClick={()=> handleLogOut()} color="error" startIcon={<LogoutIcon />}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
