a414cf322628900e114a4fe847568c865c54ead7
agusv <agusvarela5@gmail.com>
2024-01-12 01:58:33 -0300

    add(frontend): aplicacion de cloudinary y finalizacion de estilos

--- Frontend-react/src/components/Admin/Admin.jsx
+++ Frontend-react/src/components/Admin/Admin.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
+import Collapse from '@mui/material/Collapse';
+import Button from "@mui/material/Button";
+import AddIcon from '@mui/icons-material/Add';
+
 import NuevaImpresion from "../Formularios/NuevaImpresion/NuevaImpresion";
+import { Box } from "@mui/material";
+
 
 const Admin=()=>{
+
+    const [expanded, setExpanded] = useState(false)
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
+
     return(
-        <div>
-            <NuevaImpresion/>
-        </div>
+        <Box>
+            <Button onClick={handleExpandClick} startIcon={<AddIcon/>} >Agregar impresion</Button>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <NuevaImpresion/>
+            </Collapse>
+        </Box>
     )
 }
 
