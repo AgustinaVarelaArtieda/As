9cdc6161f09b67eaaef55d608c2278e54cf578ef
agusv <agusvarela5@gmail.com>
2024-01-12 22:05:40 -0300

    style(frontend): centrado e implementacion de renderizado condicional

--- Frontend-react/src/components/Carrito/Carrito.jsx
+++ Frontend-react/src/components/Carrito/Carrito.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react"
 import NavBar, {cache} from "../NavBar/NavBar"
+import SearchBar from "../SearchBar/SearchBar"
+
 import axios from "axios"
-import { NavLink } from "react-router-dom";
-import { Typography, Box, Button, IconButton, Table, TableContainer, TableBody, TableRow, TableCell, Grid, TableHead} from "@mui/material";
+
+import { Typography, Box, Button, IconButton, Table, TableContainer, TableBody, TableRow, TableCell, Grid, TableHead, Collapse} from "@mui/material";
+
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteIcon from '@mui/icons-material/Delete';
 function Carrito (){
     const [carrito, setCarrito] = useState([]);
     const usuario = cache.get("usuario")
+
+    const [expanded, setExpanded] = useState(false)
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
 
     useEffect(()=>{
         if(usuario){
@@ -43,6 +51,7 @@ function Carrito (){
         if(!usuario){
             alert("no te registraste aun!")
         }else{
+            // eslint-disable-next-line
             const response = await axios.put(`/carrito/${usuario}`, {
                 id:id,
                 solicitud: "eliminar"
@@ -87,72 +96,77 @@ function Carrito (){
     }
     useEffect(() =>{
         calcularPrecioTotal()
+        // eslint-disable-next-line
     }, [carrito])
 
     return (
         <Box>
         <NavBar/>
-        <Grid container>
-            <Grid item xs={8}>
-                <TableContainer>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="center">
-                                </TableCell>
-                                <TableCell align="center">
-                                    Nombre
-                                </TableCell>
-                                <TableCell align="center">
-                                    Precio por unidad
-                                </TableCell>
-                                <TableCell align="center">
-                                    Cantidad
-                                </TableCell>
-                                <TableCell align="center">
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {carrito.map((impresion) => (
-                                <TableRow key={impresion.id}>
-                                    <TableCell align="center">
-                                        <Box component="img" src={impresion?.imagen} alt={impresion.nombre} sx={{ width: 100, height: 100 }} />
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Typography variant="body1">{impresion.nombre}</Typography>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Typography variant="body1">{impresion.precioBase}</Typography>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Box sx={{ display: "flex", alignItems: "center" }}>
-                                            <IconButton variant="contained" aria-label="remove" color="error" onClick={() => restarHandler(impresion.id)} disabled={impresion.cantidad === 1}>
-                                                <RemoveIcon/>
-                                            </IconButton>
-                                                <Typography variant="body2">{impresion.cantidad}</Typography>
-                                            <IconButton variant="contained" color="success" aria-label="add" onClick={() => agregarHandler(impresion.id)}>
-                                                <AddIcon/>
-                                            </IconButton>
-                                        </Box>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Button variant="contained" endIcon={<DeleteIcon/>} color="error" onClick={() => eliminarImpresion(usuario.idAuth, impresion.id)}>Eliminar</Button>
-                                </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-            </Grid>
-            {carrito.length === 0 && (
-                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h1">Agrega impresiones a tu carrito!</Typography>
-                    <NavLink to="/impresiones">
-                        <Button variant="contained">Buscar impresiones</Button>
-                    </NavLink>
+        {carrito.length === 0 ? (
+                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt:10, mb:4 }}>
+                    <Typography variant="h3" align='center'>Agrega impresiones a tu carrito!</Typography>
+                    <Button variant="contained" sx={{m:2}} onClick={handleExpandClick}>Buscar impresiones</Button>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <Box sx={{minWidth:'8rem'}} >
+                        <SearchBar/>
+                        </Box>
+                    </Collapse>
                 </Box>
-            )}
+            )
+        : (    
+            <Grid container>
+                <Grid item xs={8}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        Nombre
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        Precio por unidad
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        Cantidad
+                                    </TableCell>
+                                    <TableCell align="center">
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {carrito.map((impresion) => (
+                                    <TableRow key={impresion.id}>
+                                        <TableCell align="center">
+                                            <Box component="img" src={impresion?.imagen} alt={impresion.nombre} sx={{ width: 100, height: 100 }} />
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Typography variant="body1">{impresion.nombre}</Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Typography variant="body1">{impresion.precioBase}</Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Box sx={{ display: "flex", alignItems: "center" }}>
+                                                <IconButton variant="contained" aria-label="remove" color="error" onClick={() => restarHandler(impresion.id)} disabled={impresion.cantidad === 1}>
+                                                    <RemoveIcon/>
+                                                </IconButton>
+                                                    <Typography variant="body2">{impresion.cantidad}</Typography>
+                                                <IconButton variant="contained" color="success" aria-label="add" onClick={() => agregarHandler(impresion.id)}>
+                                                    <AddIcon/>
+                                                </IconButton>
+                                            </Box>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Button variant="contained" endIcon={<DeleteIcon/>} color="error" onClick={() => eliminarImpresion(usuario.idAuth, impresion.id)}>Eliminar</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
                 <Grid item xs sx={{
                     display: "flex",
                     flexDirection: "column",
@@ -164,7 +178,8 @@ function Carrito (){
                         Comprar
                     </Button>
                 </Grid>
-        </Grid>
+            </Grid>
+            )}
         </Box>
     );
 }
