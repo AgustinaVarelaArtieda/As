ee129210c6e0f41804a96766801be03493ec731e
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2024-01-14 18:39:17 -0300

    bugs de navbar, carrito, carrusel y footer arreglados

--- Frontend-react/src/components/Footer/Footer.jsx
+++ Frontend-react/src/components/Footer/Footer.jsx
@@ -1,31 +1,33 @@
-import { Box, Typography} from "@mui/material"
+import { AppBar, Box, Toolbar, Typography} from "@mui/material"
 
 function Footer (){
     return (
-            <Box>
-                <Box sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start"
-                }} >
-                    <Typography variant = "h6" color= "textSecondary">
-                        Informacion de contacto:
-                    </Typography>
-                    <Typography variant="body2" color= "textSecondary">
-                        Cordoba, Argentina
-                    </Typography>
-                </Box>
-                <Box>
-                    <Typography variant="h5" color= "textSecondary" textAlign= "center">A's impresiones</Typography>
-                </Box>
+            <AppBar position="static" color="transparent">
+                <Toolbar sx={{justifyContent: "space-between"}}>
                     <Box sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-                }}>
-                    <Typography variant="body2" color= "textSecondary">Creado por Gusi98 y XII</Typography>
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        }} >
+                        <Typography variant = "h6">
+                            Informacion de contacto:
+                        </Typography>
+                        <Typography variant="body2">
+                            Cordoba, Argentina
+                        </Typography>
                     </Box>
-            </Box>
+                    <Box>
+                        <Typography variant="h5" textAlign= "center">A's impresiones</Typography>
+                    </Box>
+                        <Box sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center"
+                            }}>
+                        <Typography variant="body2" color= "textSecondary">Creado por Gusi98 y XII</Typography>
+                        </Box>
+                </Toolbar>
+            </AppBar>
     )
 }
 
