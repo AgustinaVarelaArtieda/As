fe3db23e2d73357bd39116c41e99c3f69c11a6af
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2024-01-20 19:13:13 -0300

    detalle arreglado

--- Frontend-react/src/views/Detalle/Detail.jsx
+++ Frontend-react/src/views/Detalle/Detail.jsx
@@ -1,7 +1,6 @@
 import {useParams} from "react-router-dom"
 import DetalleImpresion from "../../components/Detalles/DetalleImpresion";
 import NavBar from "../../components/NavBar/NavBar";
-import Footer from "../../components/Footer/Footer";
 
 export default function Detail(){
     const { id } = useParams();
