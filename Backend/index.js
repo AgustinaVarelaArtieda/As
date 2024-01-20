21fbdafbbea4b22a5003d598b7a71857fdca2b88
agusv <agusvarela5@gmail.com>
2024-01-20 18:23:36 -0300

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,16 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); //Establece la opción extended en true para permitir datos anidados en los cuerpos de las solicitudes y establece un límite de tamaño de 50 MB para los cuerpos de las solicitudes.
 app.use(bodyParser.json({ limit: '50mb' })); //También se establece un límite de tamaño de 50 MB para los cuerpos de las solicitudes.
 app.use(cookieParser()); //para analizar las cookies en las solicitudes entrantes.
-app.use(requestLoguer);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'https://as-impresiones.vercel.app'); // . Permite las solicitudes desde el origen 'http://localhost:3000' o *
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 //Midlewares para rutas
 app.use('/asImpresiones', routes);
